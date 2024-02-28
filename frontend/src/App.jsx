7056e40952b245a59d043073df8a566ba530fb4c
Saran Pariyar <spariyar05@gmail.com>
2024-02-28 15:23:04 +0545

    calling backend from frontend - Cors policy blocks

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [jokes, setJokes] = useState({})
+
+useEffect(()=>{
+  axios.get('http://localhost:3000/jokes')
+  .then((response)=>{
+    setJokes(response.data)
+  }).catch((error)=>{
+    console.log(error);
+  })
+})
 
   return (
     <>
   <h1>Chai Aur Code</h1>
+  <p>{jokes.name}</p>
+
     </>
   )
 }
